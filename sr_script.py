3c0a2fe42ac84ebf78408e112f420df23bf13615
Wei-Min Chen <weiminc@synopsys.com>
2023-08-16 22:55:21 +0800

    update script

--- sr_script.py
+++ sr_script.py
@@ -1,27 +1,55 @@
-import os
-import time
+import argparse
+import os, time
 import tensorflow_hub as hub
 from utils import*
 
 #os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
+PROCESS_PATH = os.getcwd() + "\processed"
 
 # Declaring Constants
-IMAGE_PATH = "C:\\Users\\tnwil\\Downloads\\CH12.13\\page_724.png"
 SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
 
-hr_image = utils.preprocess_image(IMAGE_PATH)
+def run(IMAGE_PATH: str):
+    hr_image = utils.preprocess_image(IMAGE_PATH)
 
-# Plotting Original Resolution image
-utils.plot_image(tf.squeeze(hr_image), title="Original Image")
-utils.save_image(tf.squeeze(hr_image), filename="Original Image")
+    # Plotting Original Resolution image
+    #utils.plot_image(tf.squeeze(hr_image), title="Original Image")
+    #utils.save_image(tf.squeeze(hr_image), filename="Original Image")
+    model = hub.load(SAVED_MODEL_PATH)
 
-model = hub.load(SAVED_MODEL_PATH)
+    fake_image = model(hr_image)
+    fake_image = tf.squeeze(fake_image)
+    
+    BASENAME = os.path.basename(IMAGE_PATH)
+    NEWNAME = os.path.join(PROCESS_PATH, BASENAME)
+    # Plotting Super Resolution Image
+    #utils.plot_image(tf.squeeze(fake_image), title="Super Resolution")
+    utils.save_image(tf.squeeze(fake_image), filename = NEWNAME)
 
-start = time.time()
-fake_image = model(hr_image)
-fake_image = tf.squeeze(fake_image)
-print("Time Taken: %f" % (time.time() - start))
+def main():
+    parser = argparse.ArgumentParser(description="SRGAN v1_2023.08.14")
+    parser.add_argument("-path", type=str, help="Path to the images")
+    args = parser.parse_args()
 
-# Plotting Super Resolution Image
-utils.plot_image(tf.squeeze(fake_image), title="Super Resolution")
-utils.save_image(tf.squeeze(fake_image), filename="Super Resolution")
+    if not os.path.exists(PROCESS_PATH):
+        os.mkdir(PROCESS_PATH)
+
+    n = 0
+    for file_name in os.listdir(args.path):
+        IMAGE_PATH = args.path
+        full_path = os.path.join(IMAGE_PATH, file_name)
+        if os.path.isfile(full_path) and full_path.endswith(".png"):
+            n = n+1
+    i = 1
+    for file_name in os.listdir(args.path):
+        IMAGE_PATH = args.path
+        full_path = os.path.join(IMAGE_PATH, file_name)
+
+        if os.path.isfile(full_path) and full_path.endswith(".png"):
+            start = time.time()
+            run(full_path)
+            print ("[{}/{}] processed, ime taken: {}".format(i, n, (time.time() - start)))
+            i = i + 1
+
+if __name__ == "__main__":
+    main()
